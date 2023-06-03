c24c8490c7381b38c3b8210fbdbdcbe5baeb34ca
Mohammed badr-eddine El housni <mel-hous@e2r4p10.1337.ma>
2023-06-03 18:03:04 +0100

    Sat Jun  3 18:03:04 +01 2023

--- NGINX/tools/self_signed_crt.sh
+++ NGINX/tools/self_signed_crt.sh
@@ -2,4 +2,8 @@
 
 mkdir /etc/openssl
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/openssl/ssl_private_key.key -out /etc/openssl/ssl_certificate.crt
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/openssl/ssl_private_key.key -out /etc/openssl/ssl_certificate.crt
+
+openssl req -x509 -newkey rsa:4096 -keyout /etc/openssl/ssl_private_key.key -out /etc/openssl/ssl_certificate.crt -days 365 -nodes -subj "/CN=localhost" &> /dev/null
+
+nginx -g "daemon  off;"
