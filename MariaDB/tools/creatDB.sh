c0be4e2ee294863e99755bb3468da236372fe487
Mohammed badr-eddine El housni <mel-hous@e2r4p10.1337.ma>
2023-06-05 16:17:48 +0100

    Mon Jun  5 16:17:48 +01 2023

--- MariaDB/tools/creatDB.sh
+++ MariaDB/tools/creatDB.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
+if [ ! -d "/var/lib/mysql/$DB_Name" ]; then
+
 service mysql start
 
 echo "CREATE DATABASE $DB_Name;" | mysql 
 
-echo "CREATE USER $username@$hostname IDENTIFIED BY $password;" | mysql
+echo "DATABASE CREATED"
 
-echo "GRANT ALL PRIVILEGES ON $DB_Name TO $username@'%';" | mysql
+echo "GRANT ALL PRIVILEGES ON $DB_Name.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+
+echo "USER CREATED"
+
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'$hostname' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+
+echo "ROOT USER CREATED"
 
 echo "FLUSH PRIVILEGES;"| mysql
 
+echo "FLUSH SUCCESSFULLY"
+
+service mysql stop
+
+fi
 
 mysqld_safe
